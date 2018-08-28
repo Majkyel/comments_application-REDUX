796a8d7ca6fe712c5f6b58bd82918a2fe5b5e6b6
Michał Kuliś <michalkulis1993@gmail.com>
2018-08-28 15:29:37 +0200

    Update Comment.js file

--- src/Comment.js
+++ src/Comment.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './Comment.css';
 
-const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, removeComment}) => <li><p>{text}</p> <span><p>votes: {votes}</p> <button onClick={() => thumbUpComment(id)}>Thumb up!</button><button onClick={() => thumbDownComment(id)}>Thumb Down!</button><button className='delete' onClick={() => removeComment(id)}>DELETE</button></span></li>;
+const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, removeComment}) => 
+    <li>
+        <p>{text}</p>
+        <p>votes: {votes}</p>
+        <button onClick={() => thumbUpComment(id)}>Thumb up!</button>
+        <button onClick={() => thumbDownComment(id)}>Thumb Down!</button>
+        <button className='delete' onClick={() => removeComment(id)}>DELETE</button>
+    </li>;
 
 export default Comment;
